26c6588123a7a90f711e6f50076fcfa5788a76ac
nm101 <melnikovsm2@gmail.com>
2022-04-12 20:04:35 +0500

    Minor refactoring + implement adequate UDP scan

--- nmap.py
+++ nmap.py
@@ -3,43 +3,30 @@ import socket
 from argparse import ArgumentParser
 from multiprocessing import Pool
 from re import match
-from struct import pack, unpack
+from struct import error, unpack
 
-DNS_TRANSACTION_ID = b'\x02\x28'
+dns_transaction_id = b'\x02\x28'
 
-DNS_PAYLOAD = DNS_TRANSACTION_ID + \
-    b'\x01\x00\x00\x01' + \
-    b'\x00\x00\x00\x00\x00\x00' + \
-    b'\x02\x65\x31\x02\x72\x75' + \
-    b'\x00\x00\x01\x00\x01'
-
-TCP_PROTO_PAYLOADS = {
-    'HTTP': b"GET aboba/\n\n",
-    'SMTP': b'EHLO',
-    'DNS': DNS_PAYLOAD,
-    'POP3': b'AUTH'
-}
-
-UDP_PROTO_PAYLOADS = {
-    'SNTP': b'\x1b' + 47 * b'\0',
-    'DNS': DNS_PAYLOAD
-}
-
-PROTO_SIGNATURES = {
-    'HTTP': lambda packet: b'HTTP/' in packet,
-    'POP3': lambda packet: packet.startswith(b'+'),
-    'DNS': lambda packet: packet.startswith(DNS_TRANSACTION_ID),
-    'SMTP': lambda packet: match(b'[0-9]{3}', packet[:3]),
-    'SNTP': lambda packet: sntp_check(packet)
-}
+dns_payload = dns_transaction_id + \
+    b'\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00' + \
+    b'\x02\x65\x31\x02\x72\x75\x00\x00\x01\x00\x01'
 
 
-def sntp_check(packet):
+def sntp_match(data):
     try:
-        unpack('!BBBb11I', packet)
+        unpack('!BBBb11I', data)
         return True
-    except Exception:
+    except error:
         return False
+
+
+proto_signatures = {
+    'DNS': lambda data: data.startswith(dns_transaction_id),
+    'HTTP': lambda data: b'HTTP/' in data,
+    'POP3': lambda data: data.startswith(b'+'),
+    'SMTP': lambda data: match(b'[0-9]{3}', data[:3]),
+    'SNTP': sntp_match
+}
 
 
 class Scanner:
@@ -48,22 +35,26 @@ class Scanner:
         self._timeout = timeout / 1000
 
     def tcp(self, port):
+        tcp_proto_payloads = (
+            ('HTTP', b"GET aboba/\n\n"),
+            ('SMTP', b'EHLO'),
+            ('DNS', b'\x00\x17' + dns_payload),
+            ('POP3', b'AUTH')
+        )
+
         socket.setdefaulttimeout(self._timeout)
-        for proto, packet in TCP_PROTO_PAYLOADS.items():
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        for proto, payload in tcp_proto_payloads:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp:
                 try:
-                    s.connect((self._host, port))
+                    tcp.connect((self._host, port))
                 except (socket.timeout, ConnectionRefusedError):
                     return port, None
                 try:
+                    tcp.send(payload)
+                    out = tcp.recv(128)
                     if proto == 'DNS':
-                        packet = pack('!H', len(packet)) + packet
-
-                    s.send(packet)
-                    packet = s.recv(128)
-                    if proto == 'DNS':
-                        packet = packet[2:]
-                    if PROTO_SIGNATURES[proto](packet):
+                        out = out[2:]
+                    if proto_signatures[proto](out):
                         return port, proto
                 except (socket.error, OSError):
                     continue
@@ -71,15 +62,42 @@ class Scanner:
 
     def udp(self, port):
         socket.setdefaulttimeout(self._timeout)
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            for proto, packet in UDP_PROTO_PAYLOADS.items():
+        if not self._udp_is_open(port):
+            return port, None
+        return port, self._udp_determine_proto(port)
+
+    def _udp_is_open(self, port):
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp:
+            with socket.socket(
+                    socket.AF_INET,
+                    socket.SOCK_RAW,
+                    socket.IPPROTO_ICMP) as icmp:
                 try:
-                    s.sendto(packet, (self._host, port))
-                    if PROTO_SIGNATURES[proto](s.recv(128)):
-                        return port, proto
+                    udp.sendto(b"whassup dude", (self._host, port))
+                    icmp.recvfrom(1024)
+                    return False
+                except socket.timeout:
+                    pass
+                except (socket.error, OSError):
+                    return False
+            return True
+
+    def _udp_determine_proto(self, port):
+        udp_proto_payloads = (
+            ('SNTP', b'\x1b' + 47 * b'\0'),
+            ('DNS', dns_payload)
+        )
+
+        for proto, payload in udp_proto_payloads:
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp:
+                try:
+                    udp.sendto(payload, (self._host, port))
+                    out = udp.recv(128)
+                    if proto_signatures[proto](out):
+                        return proto
                 except (socket.error, OSError):
                     continue
-        return port, None
+        return "Unknown"
 
 
 def main():
@@ -89,14 +107,14 @@ def main():
                         help='Destination address or domain name')
     parser.add_argument('-tcp', action='store_true', help='Scan TCP ports')
     parser.add_argument('-udp', action='store_true', help='Scan UDP ports')
-    parser.add_argument('-s', '--start_port', default=1,
-                        type=int, help='Start port to scan')
-    parser.add_argument('-e', '--end_port', default=1024,
-                        type=int, help='End port to scan')
-    parser.add_argument('-t', '--timeout', default=100,
-                        type=int, help='Response timeout in milliseconds')
-    parser.add_argument('-p', '--processes', default=4,
-                        type=int, help='Simultaneous processes count')
+    parser.add_argument('-s', '--start_port', type=int, default=1,
+                        help='Start port to scan')
+    parser.add_argument('-e', '--end_port', type=int, default=1024,
+                        help='End port to scan')
+    parser.add_argument('-t', '--timeout', type=int, default=150,
+                        help='Response timeout in milliseconds')
+    parser.add_argument('-p', '--processes', type=int, default=4,
+                        help='Simultaneous processes count')
 
     args = parser.parse_args()
     if not (args.tcp or args.udp):
@@ -110,6 +128,8 @@ def main():
     if not ip:
         return print("Invalid IP specified")
 
+    print(f"Scanning {args.host} {'' if ip == args.host else f'({ip})'}..")
+
     pool = Pool(args.processes)
     scanner = Scanner(ip, args.timeout)
 
@@ -118,13 +138,13 @@ def main():
             args.start_port, args.end_port + 1))
         for port, proto in scan:
             if proto:
-                print(f'Open TCP {port}, proto: {proto}')
+                print(f'Open {port}/tcp, proto: {proto}')
     if args.udp:
-        scan = pool.map(scanner.udp, range(
+        scan = map(scanner.udp, range(
             args.start_port, args.end_port + 1))
         for port, proto in scan:
             if proto:
-                print(f'Open UDP {port}, proto: {proto}')
+                print(f'Open {port}/udp, proto: {proto}')
 
 
 if __name__ == "__main__":
